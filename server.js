4118f665ba65f91b79df0ed3714843dc6e9ffc6a
Abdelazim Ali <abdelazim.ali.ali@gmail.com>
2020-02-10 15:29:47 +0200

    add deployment dist

--- server.js
+++ server.js
@@ -4,8 +4,17 @@ const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+const path = require('path');
 class ExpressUtility {
+
+    static createExpressApp() {
+        const app = express();
+        app.disable('x-powered-by');
+        ExpressUtility.setBodyParser(app);
+        ExpressUtility.setCORS(app);
+        ExpressUtility.setHome(app);
+        return app;
+    }
 
     static setBodyParser(app) {
         app.use(bodyParser.json());
@@ -15,13 +24,13 @@ class ExpressUtility {
     static setCORS(app) {
         app.use(cors());
     }
-    
-    static createExpressApp() {
-        const app = express();
-        app.disable('x-powered-by');
-        ExpressUtility.setBodyParser(app);
-        ExpressUtility.setCORS(app);
-        return app;
+
+    static setHome(app) {
+        app.use(express.static(path.join(__dirname, 'dist/in3-computer-store')))
+        app.get("*", function (req, res, next) {
+            console.log("enter here")
+            res.sendFile(path.join(__dirname, 'dist/in3-computer-store/index.html'));
+        });
     }
 
     static startApp(app, config) {
