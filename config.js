432b640a7912691e646ac220a70346b0240c7cc4
Abdelazim Ali <abdelazim.ali.ali@gmail.com>
2020-02-10 14:11:08 +0200

    edit port to deploy on heroku

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-    port: process.env.IN3_SERVICE_PORT || 5300,
+    port: process.env.PORT || 5300,
     name: 'in3service',
     db: process.env.IN3_SERVICE_MONGO_URL || 'mongodb://mongo:27017/in3-service',
     endpoint: 'in3service',
